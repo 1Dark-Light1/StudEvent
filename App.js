e2877ceaf399a054fd4f20133d566a98fb579469
DenysHrytsai <ppanda3553@gmail.com>
2025-11-13 21:59:29 +0100

    AddLogo1

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={require('./assets/logo.png')} // замените logo.png на имя вашего файла
+        style={styles.logo}
+      />
       <Text>StudEvent!</Text>
       <Text>Organizuj. Dolączaj. Komunikuj się!</Text>
       <StatusBar style="auto" />
